409b49b943b00ef0421ad33dbec139d3a839a869
FranAquino2110 <franaquino2110@gmail.com>
2019-06-03 13:10:11 -0300

    hicimos que funcione algo
    xd

--- visual studio xd/WindowsApplication2/Form1.vb
+++ visual studio xd/WindowsApplication2/Form1.vb
@@ -1,19 +1,21 @@
 ﻿Public Class Form1
     Dim quesito(90) As Label
     Private Sub duplicarlabels()
-        quesito(1).Left = 40
-        quesito(1).Top = 40
+
         For i = 1 To 90
             quesito(i) = New Label()
+            Me.Controls.Add(quesito(i))
+            quesito(1).Left = 20
+            quesito(1).Top = 20
             quesito(i).Visible = True
-
-            quesito(i).Width = 80
-            quesito(i).Height = 80
+            quesito(i).Width = 40
+            quesito(i).Height = 40
             quesito(i).Text = i
             If i <> 1 Then
                 quesito(i).Left = quesito(i - 1).Left + 80
                 quesito(i).Top = quesito(i - 1).Top + 80
             End If
+
         Next
     End Sub
 
