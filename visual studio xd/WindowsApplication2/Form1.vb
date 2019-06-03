fa2dd333cfcc67204720b4ac976e29fa590db39a
FranAquino2110 <franaquino2110@gmail.com>
2019-06-03 13:00:12 -0300

    aa
    agregamos otro archivo

---
+++ visual studio xd/WindowsApplication2/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Dim quesito(90) As Label
+    Private Sub duplicarlabels()
+        quesito(1).Left = 40
+        quesito(1).Top = 40
+        For i = 1 To 90
+            quesito(i) = New Label()
+            quesito(i).Visible = True
+
+            quesito(i).Width = 80
+            quesito(i).Height = 80
+            quesito(i).Text = i
+            If i <> 1 Then
+                quesito(i).Left = quesito(i - 1).Left + 80
+                quesito(i).Top = quesito(i - 1).Top + 80
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        duplicarlabels()
+    End Sub
+End Class
